99b7d6982d7d786223f2ee57fbfa95f8d5275ae8
Abhinav Arora <78192977+AbhinavArora123@users.noreply.github.com>
2022-11-17 22:38:15 +0530

    Add files via upload

---
+++ SUBSCRIBE.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x<=30){
+            cout<<"No"<<endl;
+        }
+        else{
+            cout<<"Yes"<<endl;
+        }
+    }
+	return 0;
+}
